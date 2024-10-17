1c0d755540384242d0289e0621ca025d061a978d
maar34 <world.maar@gmail.com>
2024-10-17 15:44:47 +0000

    change rate limit to 200/15min

--- server.js
+++ server.js
@@ -58,7 +58,7 @@ app.use((req, res, next) => {
 // Rate Limiting Middleware
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // Limit each IP to 100 requests per windowMs
+  max: 200, // Limit each IP to 100 requests per windowMs
   message: 'Too many requests from this IP, please try again later.'
 });
 app.use(limiter);
